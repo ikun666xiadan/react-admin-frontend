45c9f4143ed4369819502491ed0f70ae2cbefff7
Y <2893620464@qq.com>
2025-04-15 17:25:07 +0800

    feat: layout页面布局,封装侧边nav组件

---
+++ src/components/Asider/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  HomeOutlined,
+  UserOutlined,
+  ShopOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const { Sider } = Layout;
+const NavItem = [
+  {
+    key: "home",
+    icon: <HomeOutlined />,
+    label: "首页",
+  },
+  {
+    key: "goods",
+    icon: <ShopOutlined />,
+    label: "商品管理",
+  },
+  {
+    key: "user",
+    icon: <UserOutlined />,
+    label: "用户管理",
+  },
+  {
+    key: "other",
+    icon: <UnorderedListOutlined />,
+    label: "其他",
+  },
+];
+const Asider = React.memo(({ collapsed }) => {
+  const navigate = useNavigate()
+  return (
+    <Sider trigger={null} collapsible collapsed={collapsed}>
+      <div className="nav" />
+      <p style={{ color: "white", textAlign: "center", margin: "10px 0" }}>
+        通用后台管理系统
+      </p>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["home"]}
+        items={NavItem}
+        onClick={(e) => navigate(e.key)}
+      />
+    </Sider>
+  );
+});
+
+export default Asider;
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,13 +1,44 @@
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Button, Layout, theme } from "antd";
+import Asider from "../../components/Asider";
 
-function Layout() {
-    return (
-      <div className="Layout">
-        Layout
-        <Outlet></Outlet>
-      </div>
-    );
-  }
-  
-export default Layout;
-  
+const { Header, Content } = Layout;
+const LayoutApp = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout className="container">
+      <Asider collapsed={collapsed} />
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Outlet></Outlet>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default LayoutApp;
