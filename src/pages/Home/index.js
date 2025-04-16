cae8c8d6cf7a35ce4a46f6767f39427f12cdc81f
Y <2893620464@qq.com>
2025-04-16 18:57:28 +0800

    feat: Home页面用户信息实现

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,25 @@
-
+import Avatar from '../../assets/me.png'
+import { Col, Row,Card  } from 'antd';
+import "./index.css"
 
 function Home() {
     return (
-      <div className="Home">
-        Home
-      </div>
+      <Row>
+        <Col span={8}>
+          <Card className='user_info' hoverable>
+            <div className='user'>
+              <img src={Avatar} alt=''/>
+              <div className='user_admin'>
+                <h1>Admin</h1>
+                <p>超级管理员</p>
+              </div>
+            </div>
+            <div className='info'>上次登入时间:  2022-12-5</div>
+            <div className='info'>上次登入地点:  江西</div>
+          </Card>
+        </Col>
+        <Col span={16}>col-12</Col>
+      </Row>
     );
   }
   
