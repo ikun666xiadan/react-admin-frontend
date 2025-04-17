386c3c2bf9aeb673c8d5e5ad4869141165f20825
Y <2893620464@qq.com>
2025-04-17 19:34:37 +0800

    feat: user页面静态搭建,并封装table列表

---
+++ src/pages/User/TableList/index.js
@@ -0,0 +1,77 @@
+import { Table } from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Button, Popconfirm } from "antd";
+
+const dataSource = [
+  {
+    id: 1,
+    name: "胡彦斌",
+    gender: "男",
+    age: 32,
+    date: "2002-02-12",
+    phone: "1234567890",
+    address: "西湖区湖底公园1号",
+  },
+  {
+    id: 1,
+    name: "胡彦斌",
+    gender: "男",
+    age: 32,
+    date: "2002-02-12",
+    phone: "1234567890",
+    address: "西湖区湖底公园1号",
+  },
+];
+
+const columns = [
+  {
+    title: "姓名",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "性别",
+    dataIndex: "gender",
+    key: "gender",
+  },
+  {
+    title: "年龄",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "出生日期",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "手机号",
+    dataIndex: "phone",
+    key: "phone",
+  },
+  {
+    title: "住址",
+    dataIndex: "address",
+    key: "address",
+  },
+  {
+    title: "操作",
+    dataIndex: "action",
+    key: "action",
+    render: (_, record) =>
+      dataSource.length >= 1 ? (
+        <div className="action">
+          <Button type="primary" icon={<EditOutlined />}></Button>
+          <Popconfirm title="确认删除吗?">
+            <Button type="primary" danger icon={<DeleteOutlined />}></Button>
+          </Popconfirm>
+        </div>
+      ) : null,
+  },
+];
+
+const TableList = () => {
+  return <Table dataSource={dataSource} columns={columns} />;
+};
+
+export default TableList;
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,27 @@
+import { PlusOutlined } from "@ant-design/icons";
+import { Button,Input } from "antd";
+import "./index.css"
+import TableList from "./TableList";
 
+
+const {Search} = Input
 
 function User() {
-    return (
-      <div className="User">
-        User
-      </div>
-    );
+  const onSearch = (e) =>{
+    console.log(e);
+    
   }
-  
+  return (
+    <div className="User">
+      <div className="btns">
+        <Button type="primary" icon={<PlusOutlined />}>
+          新增
+        </Button>
+        <Search className="search" placeholder="请输入用户名" onSearch={onSearch} enterButton />
+      </div>
+      <TableList/>
+    </div>
+  );
+}
+
 export default User;
-  
